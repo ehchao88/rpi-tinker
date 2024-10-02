4384d3dae57f654e100ece498139d63972618b22
Eric Chao <ericchao@erics-mbp.mynetworksettings.com>
2024-10-02 00:22:05 -0400

    screen printing image

---
+++ rpi-cam-to-lcd/convert.py
@@ -0,0 +1,23 @@
+from image import *
+import lcd5110 as LCD
+import time
+import sys
+
+converter: ImageConverter = ImageConverter()
+image = converter.convert_to_grayscale('test_images/rhett.png')
+image.save('test_results/rhett_gray.png')
+
+image = converter.convert_to_grayscale_and_resize('test_images/rhett.png')
+image.save('test_results/rhett_resized.png')
+
+imageBytes = converter.get_image_bytes('test_images/rhett.png')
+print(imageBytes)
+
+screen = LCD.LCD5110()
+while True:
+    try: 
+        screen.printFullImage(imageBytes)
+        time.sleep(5)
+    except KeyboardInterrupt:
+        screen.cleanup()
+        sys.exit(0)
---
+++ rpi-cam-to-lcd/image.py
@@ -0,0 +1,26 @@
+import io
+from PIL import Image, ImageFile
+
+class ImageConverter:
+    def __init__(self):
+        self.width = 84
+        self.height = 48
+    
+    def convert_to_grayscale(self, file_path: str) -> ImageFile.ImageFile:
+        raw_image: ImageFile.ImageFile = Image.open(file_path)
+        greyscale_image: ImageFile.ImageFile = raw_image.convert('L')
+        return greyscale_image
+    
+    def convert_to_grayscale_and_resize(self, file_path: str) -> ImageFile.ImageFile:
+        raw_image: ImageFile.ImageFile = Image.open(file_path)
+        greyscale_image: ImageFile.ImageFile = raw_image.convert('L')
+        size = (self.width, self.height)
+        greyscale_image = greyscale_image.resize(size)
+        return greyscale_image
+    
+    def get_image_bytes (self, file_path: str):
+        resized_image = self.convert_to_grayscale_and_resize(file_path)
+        image_bytes = io.BytesIO()
+        resized_image.save(image_bytes, format='PNG')
+        return list(image_bytes.getvalue())
+    
