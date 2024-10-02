40f931fb39b2189b1f1c55a8cf84a2f7f43aa97b
Eric Chao <ericchao@erics-mbp.mynetworksettings.com>
2024-10-02 00:58:30 -0400

    sbitmap

--- rpi-cam-to-lcd/image.py
+++ rpi-cam-to-lcd/image.py
@@ -11,7 +11,7 @@ class ImageConverter:
         greyscale_image: ImageFile.ImageFile = raw_image.convert('L')
         return greyscale_image
     
-    def convert_to_grayscale_and_resize(self, file_path: str) -> ImageFile.ImageFile:
+    def convert_to_grayscale_and_resize(self, file_path: str):
         raw_image: ImageFile.ImageFile = Image.open(file_path)
         greyscale_image: ImageFile.ImageFile = raw_image.convert('L')
         size = (int(self.width/6) + 1, int(self.height/8) + 1)
