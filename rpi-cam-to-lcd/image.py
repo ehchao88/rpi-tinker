cac7955826e685e594a0d253d283957dda53eb79
Eric Chao <ericchao@erics-mbp.mynetworksettings.com>
2024-10-03 22:19:08 -0400

    fix bytestring

--- rpi-cam-to-lcd/image.py
+++ rpi-cam-to-lcd/image.py
@@ -8,19 +8,27 @@ class ImageConverter:
     
     def convert_to_grayscale(self, file_path: str) -> ImageFile.ImageFile:
         raw_image: ImageFile.ImageFile = Image.open(file_path)
-        greyscale_image: ImageFile.ImageFile = raw_image.convert('1')
+        greyscale_image: ImageFile.ImageFile = raw_image.convert('1', dither=None)
         return greyscale_image
     
     def convert_to_grayscale_and_resize(self, file_path: str):
         raw_image: ImageFile.ImageFile = Image.open(file_path)
-        greyscale_image: ImageFile.ImageFile = raw_image.convert('1')
+        greyscale_image: ImageFile.ImageFile = raw_image.convert('1', dither=None)
         size = (self.width, self.height)
         greyscale_image = greyscale_image.resize(size)
         return greyscale_image
     
     def get_image_bytes (self, file_path: str):
         resized_image = self.convert_to_grayscale_and_resize(file_path)
-        resized_image = resized_image.convert(mode="1")
-        resized_image = resized_image.tobitmap()
-        return resized_image
+        w,h = resized_image.size
+        bits = ''
+        for y in range(h):
+            for x in range(w):
+                if resized_image.getpixel((x,y)) == 1:
+                    bits += '1'
+                else:
+                    bits += '0'
+        byte_rep = int(bits, base=2).to_bytes(int(len(bits)/8), 'little')
+
+        return byte_rep
     
