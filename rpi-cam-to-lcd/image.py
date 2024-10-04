37c232ea5ba2ba09ed6fe69df043b4f22f6302d6
Eric Chao <ericchao@erics-mbp.mynetworksettings.com>
2024-10-03 22:52:39 -0400

    Docs

--- rpi-cam-to-lcd/image.py
+++ rpi-cam-to-lcd/image.py
@@ -1,3 +1,5 @@
+#Converts full size images to 84x48 bitmap to be compatible with Nokia 5110 LCD
+
 import io
 from PIL import Image, ImageFile
 
@@ -18,10 +20,12 @@ class ImageConverter:
         greyscale_image = greyscale_image.resize(size)
         return greyscale_image
     
+    # Converts black and white image to bitmap, with addressing compatible with Nokia 5110. 
     def get_image_bytes (self, file_path: str):
         resized_image = self.convert_to_grayscale_and_resize(file_path)
         w,h = resized_image.size
         bits = ''
+        # Screen pixels are addressed by bytes, each representing a block of 8 pixel height and 1 pixel width
         cur_y = 0
         while cur_y < h:
             for x in range(w):
@@ -35,6 +39,7 @@ class ImageConverter:
                 bits += cur_byte
             cur_y += 8
 
+        # Convert bit string to byte representation
         byte_rep = int(bits, base=2).to_bytes(int(len(bits)/8), 'little')
 
         return byte_rep
